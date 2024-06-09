bfba5a64794fe8c0213ddfadeb4eeedbc341d535
vicgriffin123 <129837311+vicgriffin123@users.noreply.github.com>
2024-06-09 21:10:15 +0300

    more on trips section

--- src/pages/Trips/Trips.jsx
+++ src/pages/Trips/Trips.jsx
@@ -20,6 +20,75 @@ function Trips() {
             </div>
             <h3 className="card--name">Honey moon</h3>
             <span className="card--details">amount</span>
+
+            <div className="info">
+                <div className="info--icon">
+                    <i></i>
+                </div>
+                
+                <div className="info--border">
+                    <div className="info--place">
+                        <img src={honey_img} alt="" className='img'/>
+                    </div>
+                </div>
+                <div className="info--data">
+                    <h3 className="info--name">Honey moon</h3>
+                    <span className="info--details">Lorem ipsum dolor sit amet consectetur.</span>
+                    <span className="info--location">nairobi kenya</span>
+                </div>
+            </div>
+        </div>
+        <div className="card-board">
+            <div className="card__border">
+                <div className="card__place">
+                    <img src={honey_img} alt="" className="card__img" />
+                </div>
+            </div>
+            <h3 className="card--name">Honey moon</h3>
+            <span className="card--details">amount</span>
+
+            <div className="info">
+                <div className="info--icon">
+                    <i></i>
+                </div>
+                
+                <div className="info--border">
+                    <div className="info--place">
+                        <img src={honey_img} alt="" className='img'/>
+                    </div>
+                </div>
+                <div className="info--data">
+                    <h3 className="info--name">Honey moon</h3>
+                    <span className="info--details">Lorem ipsum dolor sit amet consectetur.</span>
+                    <span className="info--location">nairobi kenya</span>
+                </div>
+            </div>
+        </div>
+        <div className="card-board">
+            <div className="card__border">
+                <div className="card__place">
+                    <img src={honey_img} alt="" className="card__img" />
+                </div>
+            </div>
+            <h3 className="card--name">Honey moon</h3>
+            <span className="card--details">amount</span>
+
+            <div className="info">
+                <div className="info--icon">
+                    <i></i>
+                </div>
+                
+                <div className="info--border">
+                    <div className="info--place">
+                        <img src={honey_img} alt="" className='img'/>
+                    </div>
+                </div>
+                <div className="info--data">
+                    <h3 className="info--name">Honey moon</h3>
+                    <span className="info--details">Lorem ipsum dolor sit amet consectetur.</span>
+                    <span className="info--location">nairobi kenya</span>
+                </div>
+            </div>
         </div>
         </div>
 
