385f228b6d50b080de1f93fa06f73c68f6e7a963
vicgriffin123 <129837311+vicgriffin123@users.noreply.github.com>
2024-06-09 21:46:00 +0300

    more on trips section

--- src/pages/Trips/Trips.jsx
+++ src/pages/Trips/Trips.jsx
@@ -43,8 +43,7 @@ function Trips() {
             </div>
         </div>
         </div>
-
-        
+      
         <div className="container">
         <div className="card-board">
             <div className="card__border">
@@ -52,36 +51,7 @@ function Trips() {
                     <img src={honey_img} alt="" className="card__img" />
                 </div>
             </div>
-            <h3 className="card--name">wellness retreats and culinary tours</h3>
-            <span className="card--details">amount</span>
-
-            <div className="info">
-                <div className="info--icon">
-                    <i></i>
-                </div>
-                
-                <div className="info--border">
-                    <div className="info--place">
-                        <img src={honey_img} alt="" className='img'/>
-                    </div>
-                </div>
-                <div className="info--data">
-                    <h3 className="info--name">beach holidays</h3>
-                    <span className="info--details">Lorem ipsum dolor sit amet consectetur.</span>
-                    <span className="info--location">nairobi kenya</span>
-                </div>
-            </div>
-        </div>
-        </div>
-        
-        <div className="container">
-        <div className="card-board">
-            <div className="card__border">
-                <div className="card__place">
-                    <img src={honey_img} alt="" className="card__img" />
-                </div>
-            </div>
-            <h3 className="card--name"> luxury vacations</h3>
+            <h3 className="card--name">eco-tours</h3>
             <span className="card--details">amount</span>
 
             <div className="info">
@@ -124,7 +94,7 @@ function Trips() {
                     </div>
                 </div>
                 <div className="info--data">
-                    <h3 className="info--name">cultural tours</h3>
+                    <h3 className="info--name">wildlife safaris</h3>
                     <span className="info--details">Lorem ipsum dolor sit amet consectetur.</span>
                     <span className="info--location">nairobi kenya</span>
                 </div>
@@ -139,7 +109,7 @@ function Trips() {
                     <img src={honey_img} alt="" className="card__img" />
                 </div>
             </div>
-            <h3 className="card--name"> family</h3>
+            <h3 className="card--name">family</h3>
             <span className="card--details">amount</span>
 
             <div className="info">
@@ -153,7 +123,7 @@ function Trips() {
                     </div>
                 </div>
                 <div className="info--data">
-                    <h3 className="info--name">adventure</h3>
+                    <h3 className="info--name">family</h3>
                     <span className="info--details">Lorem ipsum dolor sit amet consectetur.</span>
                     <span className="info--location">nairobi kenya</span>
                 </div>
